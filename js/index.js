a2e9bca6833ce3f78e1a264f40150c605186fd3f
Ilia <galietskiy2@gmail.com>
2021-07-12 17:23:59 +0300

    add javascript menu in header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // DESCKTOP HEADER MENU OPEN AND CLOSE
+const openMenu = document.querySelector('.header__menu__img-open').onclick = open;
+const menu = document.querySelector('.header__window-menu');
+function open(){
+    menu.classList.toggle('windows-close')
+}
