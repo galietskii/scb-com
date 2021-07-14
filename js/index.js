26d85efb1cdeb9c3d8139c3db9809091e3fdbbff
Ilia <galietskiy2@gmail.com>
2021-07-14 16:48:59 +0300

    add left and right asides block

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-// DESCKTOP HEADER MENU OPEN AND CLOSE
+// DESKTOP HEADER MENU OPEN AND CLOSE
 const openMenu = document.querySelector('.header__menu__img-open').onclick = open;
 const menu = document.querySelector('.header__window-menu');
 function open(){
     menu.classList.toggle('windows-close')
+}
+// RECORDS BLOCK OFF SUB MENU
+const rollUp = document.querySelector('.records__menu-close').onclick = rollUpMenu;
+const subMenu = document.querySelector('.records__submenu');
+function rollUpMenu() {
+    subMenu.classList.toggle('records__close');
 }
